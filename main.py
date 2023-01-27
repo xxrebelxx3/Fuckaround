91f6179ad371d1c82c25bca4559cdc81cce917d2
xxrebelxx3 <konopa.devin@gmail.com>
2023-01-27 18:04:46 -0500

    Attempting to create a function to consume potion or check stats based on user input

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from character import Character
 
-player = Character(name=input("Choose a name for your character: "), attack=20, health=200, defense=1, inv_potion=10)
+def Global_choice(res):
+    if res == "P":
+        player.Consume_potion
+    if res == "CS":
+        player.Check_stats
+        
+player = Character(name=input("Choose a name for your character: "), attack=20, hp=200, defense=1, inv_potion=10)
 enemy1 = Character(name="Killa", attack=5, defense=5)
 
 print(player.__dict__)
@@ -8,8 +14,9 @@ print(enemy1.__dict__)
 
 player.Dmg(100)
 player.Check_stats()
-choice = input("Press P to drink a potion")
-
-if choice == "P":
-    player.Consume_potion()
-player.Check_stats()
+res = input("Enter \"P\" or \"POTION\" to drink a potion: ")
+print(res)
+Global_choice("P")
+res = input("Enter \"CS\" or \"CHECKSTATS\" to check player statistics: ")
+print(res)
+Global_choice("CS")
