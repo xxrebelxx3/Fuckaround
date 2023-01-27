7cb5fb06d9a9807ed6eb4e656c293da39a0ad815
xxrebelxx3 <konopa.devin@gmail.com>
2023-01-27 18:41:49 -0500

    Added function for global input. Check potions, stats, etc. Currently using a global variable for this and might change but works for now

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from character import Character
 
-def Global_choice(res):
+choice = "a"
+
+def Global_choice(res=input()):
     if res == "P":
-        player.Consume_potion
+        player.Consume_potion()
     if res == "CS":
-        player.Check_stats
-        
+        player.Check_stats()
+    else:
+        global choice 
+        choice = res
+    
 player = Character(name=input("Choose a name for your character: "), attack=20, hp=200, defense=1, inv_potion=10)
 enemy1 = Character(name="Killa", attack=5, defense=5)
 
@@ -14,9 +19,13 @@ print(enemy1.__dict__)
 
 player.Dmg(100)
 player.Check_stats()
-res = input("Enter \"P\" or \"POTION\" to drink a potion: ")
-print(res)
-Global_choice("P")
-res = input("Enter \"CS\" or \"CHECKSTATS\" to check player statistics: ")
-print(res)
-Global_choice("CS")
+
+res = input("Enter \"P\" to consume a potion or \"CS\" to check player statistics: ")
+if res == "P":
+    player.Consume_potion()
+if res == "CS":
+    player.Check_stats()
+
+Global_choice(input("Enter \"P\" to consume a potion or \"CS\" to check player statistics: "))
+if choice == "L":
+    print("You go left")
